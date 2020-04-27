1a0bcac82528c11acf7df901f4b09eee3baf13d4
jenkin <jenkin.ahorn@gmail.com>
2020-04-27 17:34:22 +0800

    add

---
+++ getKNumber.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+
+using namespace std;
+
+typedef struct Node
+{
+	int value;
+	struct Node *next;
+}Node;
+int main() {
+
+	int n, k;
+	cin >> n >> k;
+	Node* head = new Node();
+	
+	bool first = true;
+	while (n) {
+		n--;
+		int a;
+		cin >> a;
+		int* arr = new int[a];
+		int m = a;
+		int t = 0;
+		while (t<a) {
+			cin >> arr[t];
+			t++;
+		}
+
+		Node* p = head;
+		if (first) {
+			while (m) {
+				Node* node = new Node();
+				node->value = arr[m - 1];
+				node->next = nullptr;
+				
+				p->next = node;
+				p = p->next;
+
+				m--;
+			}
+			first = false;
+		}
+		while (m) {
+			
+			Node* node = new Node();
+			node->value = arr[m - 1];
+			node->next = nullptr;
+			
+			while (p ->next != nullptr&&p->next->value > arr[m -1]) {
+				p = p->next;
+			}
+			if (p->next == nullptr&&p->value > arr[m - 1]) {
+				p->next = node;
+			}
+			else {
+				node->next = p->next;
+				p->next = node;
+			}
+
+			p = node;
+			m--;
+		}
+	}
+
+	Node* q = head->next;
+	while (k) {
+		cout << q->value << " ";
+		k--;
+		q = q->next;
+	}
+
+	return 0;
+}
---
+++ getNode.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+
+using namespace std;
+
+void getNode(int x, int k);
+
+int main() {
+
+	int q;
+	cin >> q;
+	int x, k;
+	while (q) {
+		cin >> x >> k;
+		q--;
+		getNode(x, k);
+	}
+	return 0;
+}
+
+void getNode(int x, int k) {
+
+	if (x<1 || k<1) return;
+
+	int t = 1;
+	int n = 1;
+	while (n << 1 <= x) {
+		n = n << 1;
+		t++;
+	}
+
+	int m = t - k;
+	if (m <= 0) {
+		cout << "-1" << endl;
+		return;
+	}
+	int r = x;
+	r = r >> m;
+	cout << r << endl;
+}
